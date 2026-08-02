4be38351f4cc031e19bf584b7bb6f57f85395e13
Antônio Camargo <apcamargo@users.noreply.github.com>
2026-08-02 16:02:50 -0300

    Add shell completions

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -o xtrace -o nounset -o pipefail -o errexit
+
 if [[ "${target_platform}" == linux-* ]]; then
     export OPENSSL_DIR=$PREFIX
 fi
@@ -11,3 +13,8 @@ cargo-bundle-licenses \
 cargo install --no-track --locked --root "$PREFIX" --path crates/tytanic
 
 "$STRIP" "$PREFIX/bin/tt"
+
+mkdir -p $PREFIX/share/zsh/site-functions $PREFIX/share/bash-completion/completions $PREFIX/share/fish/vendor_completions.d
+$PREFIX/bin/tt util completion bash > $PREFIX/share/bash-completion/completions/tt
+$PREFIX/bin/tt util completion zsh > $PREFIX/share/zsh/site-functions/_tt
+$PREFIX/bin/tt util completion fish > $PREFIX/share/fish/vendor_completions.d/tt.fish
